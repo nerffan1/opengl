e7b3aea24fb951dd7a21b92d346ce4e2d427f835
nerffan1 <angelemvs@gmail.com>
2025-10-21 17:36:02 -0400

    Input small fix Remove long comment. Already implemented.

--- opengl/input.h
+++ opengl/input.h
@@ -7,36 +7,6 @@ extern float r;
 extern float g;
 extern float b;
 
-
-//void processInput(GLFWwindow* window)
-//{
-//    glfwPollEvents();
-//
-//    //Check actors that have the module
-//
-//    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-//        glfwSetWindowShouldClose(window, true);
-//
-//    if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
-//        b = fminf(b + 0.0005f, 1.0f); // Increase color, clamp to 1.0
-//
-//    if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
-//        b = fmaxf(b - 0.0005f, 0.0f); // Decrease color, clamp to 0.0
-//
-//    if (glfwGetKey(window, GLFW_KEY_HOME) == GLFW_PRESS)
-//        g = fminf(g + 0.0005f, 1.0f); // Increase color, clamp to 1.0
-//
-//    if (glfwGetKey(window, GLFW_KEY_END) == GLFW_PRESS)
-//        g = fmaxf(g - 0.0005f, 0.0f); // Decrease color, clamp to 0.0
-//
-//    if (glfwGetKey(window, GLFW_KEY_INSERT) == GLFW_PRESS)
-//        r = fminf(r + 0.0005f, 1.0f); // Increase color, clamp to 1.0
-//
-//    if (glfwGetKey(window, GLFW_KEY_DELETE) == GLFW_PRESS)
-//        r = fmaxf(r - 0.0005f, 0.0f); // Decrease color, clamp to 0.0
-//}
-
-
 class Input
 {
 public:
